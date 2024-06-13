fb82d97f312f31ed846e3f24fb7e458b45ed212c
JoseAntonioMartinez2525 <bubbleteahyung@gmail.com>
2024-06-13 09:17:58 -0700

    Eliminación de las tablas de la actividad 3 en la vista welcome

--- formato-evaluacion/resources/views/docencia.blade.php
+++ formato-evaluacion/resources/views/docencia.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Formato de Evaluación</title>
+    <title>Formato de Evaluación docente</title>
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
