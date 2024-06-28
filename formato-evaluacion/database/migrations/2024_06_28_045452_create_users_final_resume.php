3129f3092c055d63082a42c21a7becbc31a6553e
JoseAntonioMartinez2525 <bubbleteahyung@gmail.com>
2024-06-27 22:07:02 -0700

    Agregar migracion del reumen total para el formulario del blade resumen

---
+++ formato-evaluacion/database/migrations/2024_06_28_045452_create_users_final_resume.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users_final_resume', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('email');
+            $table->foreign('email')->references('email')->on('users')->onDelete('cascade');
+            $table->integer('comision_actividad_1_total');
+            $table->integer('comision_actividad_2_total');
+            $table->integer('comision_actividad_3_total');
+            $table->integer('total_puntaje');
+            $table->string('minima_calidad');
+            $table->string('minima_total');
+            $table->string('persona_evaluadora');
+            $table->binary('firma');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users_final_resume');
+    }
+};
