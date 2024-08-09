d44849f7ac73256a32603a22a6b982635611ff17
JoseAntonioMartinez2525 <bubbleteahyung@gmail.com>
2024-08-09 11:27:32 -0700

    UPDATE fix responsivity

--- formato-evaluacion/resources/views/comision_dictaminadora.blade.php
+++ formato-evaluacion/resources/views/comision_dictaminadora.blade.php
@@ -9,7 +9,7 @@ $newLocale = str_replace('_', '-', $locale);
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Formato de Evaluación docente</title>
+    <title></title>
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
