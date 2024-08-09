2e4d1a3ef74e43def2773c8efd42fd52bfb537af
JoseAntonioMartinez2525 <bubbleteahyung@gmail.com>
2024-08-09 10:55:22 -0700

    UPDATE header component

--- formato-evaluacion/resources/views/components/general-header.blade.php
+++ formato-evaluacion/resources/views/components/general-header.blade.php
@@ -1,5 +1,5 @@
 <div style="text-align: center; margin-bottom: 50px;">
-    <img src="{{ $logo }}" alt="Logo UABCS" style="width: 150px; height: auto; margin-bottom: 10px;margin-left: -1100px; margin-top: 30px;">
-    <h1 style="margin: 0; font-size: 24px;margin-top: -90px;">{{ $title }}</h1>
-    <h3 style="margin: 0; font-size: 18px; color: #666;">{{ $subtitle }}</h3>
+    <img src="{{ $logo }}" alt="Logo UABCS" style="width: 150px; height: auto; margin-bottom: 10px;margin-left: -550px; margin-top: 30px;">
+    <h1 style="margin: 0; font-size: 24px; margin-top: -90px; margin-left:200px;">{{ $title }}</h1>
+    <h3 style="margin: 0; font-size: 18px; color: #666; margin-left:200px;">{{ $subtitle }}</h3>
 </div>
