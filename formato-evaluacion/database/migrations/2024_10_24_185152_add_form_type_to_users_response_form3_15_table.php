ce28b912b88899eeb05e9dc513f92d2d5ecc662d
JoseAntonioMartinez2525 <bubbleteahyung@gmail.com>
2024-10-24 11:54:33 -0700

    Update docentes's forms

---
+++ formato-evaluacion/database/migrations/2024_10_24_185152_add_form_type_to_users_response_form3_15_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users_response_form3_15', function (Blueprint $table) {
+            $table->string('form_type')->default('form3_15');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users_response_form3_15', function (Blueprint $table) {
+            //
+        });
+    }
+};
