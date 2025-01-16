b2ff12d15c75169bd940b7a42271ec58a800658e
JoseAntonioMartinez2525 <bubbleteahyung@gmail.com>
2025-01-16 11:06:32 -0700

    Update

--- formato-evaluacion/resources/views/dynamic_forms.blade.php
+++ formato-evaluacion/resources/views/dynamic_forms.blade.php
@@ -183,57 +183,6 @@ $newLocale = str_replace('_', '-', $locale);
 
     <script>
 
-
-        function handleClick(event) {
-            var currentTarget = event.currentTarget;
-            // Use the event data here. 
-            console.log('Button clicked: ' + currentTarget.getAttribute('data-id'));
-        } document.addEventListener('DOMContentLoaded', onload);
-        function onChange() {
-            // Obtener los valores de los inputs
-            const puntajePosgrado = parseFloat(document.getElementById("horasPosgrado").value);
-            const puntajeSemestre = parseFloat(document.getElementById("horasSemestre").value);
-            const h = parseFloat(document.querySelector('#hoursText'));
-
-            // Realizar los cálculos
-            const dsePosgrado = puntajePosgrado * 8.5;
-            const dseSemestre = puntajeSemestre * 8.5;
-            const hora = (dsePosgrado + dseSemestre);
-
-            // Actualizar el contenido de las etiquetas <label>
-            document.getElementById("DSE").innerText = dsePosgrado;
-            document.getElementById("DSE2").innerText = dseSemestre;
-
-            // Mostrar los valores actualizados en la consola
-            console.log(dsePosgrado);
-            console.log(dseSemestre);
-
-            const minimo = minWithSum(dsePosgrado, dseSemestre);
-
-            document.getElementById("hoursText").innerText = minimo;
-            console.log(minimo);
-
-
-        }
-
-
-        function hayObservacion(indiceActividad) {
-            var selectEscala = document.getElementById('selectEscala' + indiceActividad);
-            var selectActividad = document.getElementById('selectActividad' + indiceActividad);
-            var inputObservacion = document.getElementById('observacion' + indiceActividad);
-            var mensajeObservacion = document.getElementById('mensajeObservacion' + indiceActividad);
-
-            if (selectActividad.value != 0 && selectEscala.value != 0) {
-                mensajeObservacion.textContent = 'Observación: ' + inputObservacion.value;
-                mensajeObservacion.style.display = 'block';
-                return true;
-            } else {
-                mensajeObservacion.style.display = 'none';
-                return false;
-            }
-        }
-
-
         const nav = document.querySelector('nav');
         let lastScrollLeft = 0; // Variable to store the last horizontal scroll position
 
@@ -251,165 +200,9 @@ $newLocale = str_replace('_', '-', $locale);
 
             lastScrollLeft = currentScrollLeft <= 0 ? 0 : currentScrollLeft; // For Mobile or negative scrolling
         });
-
-
-
-        // Function to check if there is an observation for a specific activity
-        function hayObservacion(actividad) {
-            const obs = document.querySelector(`#obs${actividad}`).value;
-            return obs.trim() !== '';
-        }
-
-        function minWithSum(value1, value2) {
-            const sum = value1 + value2;
-            return Math.min(sum, 200);
-
-
-        }
-
-        function min40(...values) {
-            const sum40 = values.reduce((acc, val) => acc + val, 0);
-            return Math.min(sum40, 40);
-        }
-
-        function min30(...values) {
-            const sum30 = values.reduce((acc, val) => acc + val, 0);
-            return Math.min(sum30, 30);
-        }
-
-        function subtotal(value1, value2) {
-            const st = value1 * value2;
-            return st;
-        }
-
-        function min60(...values) {
-            const sum60 = values.reduce((acc, val) => acc + val, 0);
-            return Math.min(sum60, 60);
-        }
-
-        function minWithSumThree(value1, value2, value3, value4) {
-            const ms = value1 + value2 + value3 + value4;
-            return Math.min(ms, 100);
-        }
-
-        function min50(...values) {
-            const ms = values.reduce((acc, val) => acc + val, 0);
-            return Math.min(ms, 50);
-        }
-
-        function minWithSumThreeFive(value1, value2) {
-            const ms = value1 + value2;
-            return Math.min(ms, 75);
-        }
-
-        function minTutorias() {
-            // convert the arguments object to an array
-            const values = Array.from(arguments);
-
-            // use reduce to sum the values
-            const ms = values.reduce((acc, current) => {
-                return acc + current;
-            }, 0);
-
-            // return the minimum of ms and 200
-            return Math.min(ms, 200);
-        }
-
-        function min700(...values) {
-            const ms = values.reduce((acc, val) => acc + val, 0);
-            return Math.min(ms, 700);
-        }
-
         // Función para actualizar el objeto data con los valores de los campos del formulario
         function actualizarData() {
             data[this.id] = this.value;
-        }
-
-        document.addEventListener('DOMContentLoaded', function () {
-            const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-            async function submitForm(url, formId) {
-                // Get form data
-                let formData = {};
-                let gridOptions = {};
-                let form = document.getElementById(formId);
-                // Ensure the form element exists
-                if (!form) {
-                    console.error(`Form with id "${formId}" not found.`);
-                    return;
-                }
-
-                //Recoge los datos dependiendo del formulario actual
-                switch (formId) {
-                    case 'form1':
-                        formData['convocatoria'] = form.querySelector('input[name="convocatoria"]').value;
-                        formData['periodo'] = form.querySelector('input[name="periodo"]').value;
-                        formData['nombre'] = form.querySelector('input[name="nombre"]').value;
-                        formData['area'] = form.querySelector('select[name="area"]').selectedOptions[0].textContent;
-                        formData['departamento'] = form.querySelector('select[name="departamento"]').selectedOptions[0].textContent;
-                        break;
-
-                    case 'form2':
-                        formData['user_id'] = form.querySelector('input[name="user_id"]').value;
-                        formData['email'] = form.querySelector('input[name="email"]').value;
-                        formData['horasActv2'] = form.querySelector('input[name="horasActv2"]').value;
-                        formData['puntajeEvaluar'] = form.querySelector('input[name="puntajeEvaluar"]').value;
-                        //formData['comision1'] = form.querySelector('input[name="comision1"]').value;
-                        formData['obs1'] = form.querySelector('input[name="obs1"]').value;
-                        break;
-
-                    case 'form2_2':
-                        formData['user_id'] = form.querySelector('input[name="user_id"]').value;
-                        formData['email'] = form.querySelector('input[name="email"]').value;
-                        let hoursLabel = form.querySelector('label[id="hoursText"]');
-                        //let actv2ComisionLabel = form.querySelector('td[id="actv2Comision"]');
-
-                        if (!hoursLabel) {
-                            console.error('Label with id "hoursText" not found.');
-                        } else {
-                            formData['hours'] = hoursLabel.innerText;
-                        }
-
-
-
-                        formData['obs2'] = form.querySelector('input[name="obs2"]').value;
-                        formData['obs2_2'] = form.querySelector('input[name="obs2_2"]').value;
-                        break;
-
-                }
-                console.log('Form data:', formData);
-
-
-                try {
-                    let response = await fetch(url, {
-                        method: 'POST',
-                        headers: {
-                            'X-CSRF-TOKEN': csrfToken,
-                            'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify(formData),
-                    });
-
-                    let responseText = await response.text(); // Obtener el texto de la respuesta
-                    console.log('Raw response from server:', responseText);
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                    }
-
-                    let data = JSON.parse(responseText);
-                    console.log('Response received from server:', data);
-                } catch (error) {
-                    console.error('There was a problem with the fetch operation:', error);
-                }
-            }
-
-            window.submitForm = submitForm;
-        });
-
-
-        // Función para actualizar el label en el footer con la convocatoria y periodo de evaluación
-        function actualizarLabelConvocatoriaPeriodo(convocatoria, periodo) {
-            const label = document.getElementById('convocatoriaPeriodoLabel');
-            label.textContent = `Convocatoria: ${convocatoria}, Período: ${periodo}`;
         }
 
 
@@ -466,7 +259,7 @@ $newLocale = str_replace('_', '-', $locale);
                 input.setAttribute('readonly', true);
                 input.style.backgroundColor = '#353e4e'; 
                 // Fondo deshabilitado
-                /*
+               
                 const puntajeMaximo = elemento.value;
                 elemento.style.backgroundColor = '#353e4e'; 
 
@@ -496,13 +289,13 @@ $newLocale = str_replace('_', '-', $locale);
                         alert('Hubo un error al actualizar el puntaje máximo.');
                     });*/
             }
-/*
+
             // Actualiza el puntaje máximo dinámicamente
             function actualizarPuntajeMaximo(valor) {
                 puntajeMaximoGlobal = valor;
                 console.log('Puntaje máximo actualizado:', puntajeMaximoGlobal);
             }
-*/
+
     // Genera los subencabezados dinámicamente
     function generateSubheaders(numColumns) {
         const subheader = document.createElement('tr');
