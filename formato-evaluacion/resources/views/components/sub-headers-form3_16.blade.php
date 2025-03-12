eee8eb99bb6e563c6eca1c6cbc612da6e5feb1c5
JoseAntonioMartinez2525 <bubbleteahyung@gmail.com>
2025-03-12 12:06:36 -0700

    Add component sub-headers_form3_16

---
+++ formato-evaluacion/resources/views/components/sub-headers-form3_16.blade.php
@@ -0,0 +1,34 @@
+<thead>
+    <tr>
+        <th scope="col" colspan=3>Actividad</th>
+        <th class="table-ajust" scope="col"></th>
+        <th class="table-ajust" scope="col"></th>
+        <th class="table-ajust" scope="col"></th>
+        <th class="table-ajust" scope="col"></th>
+        <th class="table-ajust cd" scope="col">Puntaje a evaluar</th>
+        <th class="table-ajust cd" scope="col">Puntaje de la Comisión Dictaminadora</th>
+    </tr>
+</thead>
+<thead>
+    <tr>
+        <th id="seccion3_16" class="acreditacion" colspan=7> 3.16 Actividades de arbitraje,
+            revisión,
+            correción y edición
+        </th>
+        <th class="score3_16" id="score3_16_{{ $componentIndex }}">0</th>
+        <th class="comision3_16" id="comision3_16_{{ $componentIndex }}">0</th>
+        <th class="acreditacion" scope="col">Observaciones</th>
+    </tr>
+</thead>
+<thead>
+    <tr>
+        <th class="acreditacion">Incisos</th>
+        <th class="acreditacion" colspan=1>Actividad</th>
+        <th class="acreditacion">Nivel</th>
+        <th class="acreditacion">Puntaje</th>
+        <th class="acreditacion">Cantidad</th>
+        <th></th>
+        <th></th>
+        <th class="acreditacion">Subtotal</th>
+    </tr>
+</thead>
--- formato-evaluacion/resources/views/form3_16.blade.php
+++ formato-evaluacion/resources/views/form3_16.blade.php
@@ -128,40 +128,7 @@ $user_identity = $user->id;
                         </th>
                     </tr>
                 </thead>
-                <thead>
-                    <tr>
-                        <th scope="col" colspan=3>Actividad</th>
-                        <th class="table-ajust" scope="col"></th>
-                        <th class="table-ajust" scope="col"></th>
-                        <th class="table-ajust" scope="col"></th>
-                        <th class="table-ajust" scope="col"></th>
-                        <th class="table-ajust cd" scope="col">Puntaje a evaluar</th>
-                        <th class="table-ajust cd" scope="col">Puntaje de la Comisión Dictaminadora</th>
-                    </tr>
-                </thead>
-                <thead>
-                    <tr>
-                        <th id="seccion3_16" class="acreditacion" colspan=7> 3.16 Actividades de arbitraje,
-                            revisión,
-                            correción y edición
-                        </th>
-                        <th id="score3_16">0</th>
-                        <th id="comision3_16">0</th>
-                        <th class="acreditacion" scope="col">Observaciones</th>
-                    </tr>
-                </thead>
-                <thead>
-                    <tr>
-                        <th class="acreditacion">Incisos</th>
-                        <th class="acreditacion" colspan=1>Actividad</th>
-                        <th class="acreditacion">Nivel</th>
-                        <th class="acreditacion">Puntaje</th>
-                        <th class="acreditacion">Cantidad</th>
-                        <th></th>
-                        <th></th>
-                        <th class="acreditacion">Subtotal</th>
-                    </tr>
-                </thead>
+                <x-sub-headers-form3-16 :componentIndex="0" />
                 <tbody>
                     <tr>
                         <td>a)</td>
