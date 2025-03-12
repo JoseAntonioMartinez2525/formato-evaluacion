0c926bc0ce19db63b520644724ef4cc2bb3e559f
JoseAntonioMartinez2525 <bubbleteahyung@gmail.com>
2025-03-12 11:06:33 -0700

    Add component sub-headers-form3_12

---
+++ formato-evaluacion/resources/views/components/sub-headers-form3_12.blade.php
@@ -0,0 +1,38 @@
+<thead>
+    <tr>
+        <th scope="col" colspan=3>Actividad</th>
+        <th class="table-ajust" scope="col"></th>
+        <th class="table-ajust" scope="col"></th>
+        <th class="table-ajust" scope="col"></th>
+        <th class="table-ajust" scope="col"></th>
+        <th class="table-ajust" scope="col"></th>
+        <th class="table-ajust cd" scope="col">Puntaje a evaluar</th>
+        <th class="table-ajust cd" scope="col">Puntaje de la Comisión Dictaminadora</th>
+    </tr>
+</thead>
+<thead>
+    <tr>
+        <th id="seccion3_12" class="acreditacion" colspan=7>3.12 Publicaciones de
+            investigación
+            relacionadas con el
+            contenido
+            de los PE que imparte el docente</th>
+        <th></th>
+        <th class="score3_12" id="score3_12__{{ $componentIndex }}">0</th>
+        <th class="comision3_12" id="comision3_12__{{ $componentIndex }}">0</th>
+        <th class="acreditacion" scope="col">Observaciones</th>
+    </tr>
+</thead>
+<thead>
+    <tr>
+        <th class="acreditacion">Incisos</th>
+        <th class="acreditacion">Actividad</th>
+        <th class="acreditacion">Obra</th>
+        <th class="acreditacion">Nivel</th>
+        <th class="acreditacion">Puntaje</th>
+        <th class="acreditacion">Cantidad</th>
+        <th></th>
+        <th></th>
+        <th class="acreditacion">Subtotal</th>
+    </tr>
+</thead>
