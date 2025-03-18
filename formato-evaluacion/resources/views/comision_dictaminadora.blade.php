937b68960463a9d2281dc84c88e5ac9a880d1b47
JoseAntonioMartinez2525 <bubbleteahyung@gmail.com>
2025-03-18 11:05:13 -0700

    Update

--- formato-evaluacion/resources/views/comision_dictaminadora.blade.php
+++ formato-evaluacion/resources/views/comision_dictaminadora.blade.php
@@ -25,78 +25,45 @@ $newLocale = str_replace('_', '-', $locale);
 <body class="font-sans antialiased">
     @auth
         @if(Auth::user()->user_type === 'dictaminador')
+        <x-nav-menu :user="Auth::user()"/>
+        <x-general-header />
+        <button id="toggle-dark-mode" class="btn btn-secondary printButtonClass" style="margin-left: 100px;"><i class="fa-solid fa-moon"></i>&nbspModo Obscuro</button>
 
-                    <nav class="nav flex-column" style="padding-top: 50px; height: 900px; background-color: #afc7ce;">
-                        <div class="nav-header" style="display: flex; align-items: center; padding-top: 50px;">
-                            <li class="nav-item">
-                                <a class="nav-link disabled" href="#">
-                                    <i class="fa-solid fa-user"></i>{{ Auth::user()->email }}
-                                </a>
-                            </li>
-                            <li style="list-style: none; margin-right: 20px;">
-                                <a href="{{ route('login') }}">
-                                    <i class="fas fa-power-off" style="font-size: 24px;" name="cerrar_sesion"></i>
-                                </a>
-                            </li>
-                        </div>
-                            <li class="nav-item">
-                                <a class="nav-link active enlaceSN" style="width: 200px;" href="{{ route('rules') }}">Artículo 10 REGLAMENTO PEDPD</a>
-                            </li>
-                            <li class="nav-item">
-                            @if (Auth::user()->user_type === 'docente')
-                                <a class="nav-link active enlaceSN" style="width: 200px;" href="{{route('docencia')}}">Actividades 3. Calidad en la docencia</a>
-                            @endif
-                            </li>
-                            <li class="nav-item">
-                            @if (Auth::user()->user_type !== 'docente')
-                                <a class="nav-link active enlaceSN" style="width: 200px;" href="{{ route('resumen_comision') }}">Resumen (A ser llenado por la Comisión del PEDPD)</a>
-                            @endif
-                            </li><br>
-                            <li id="jsonDataLink" class="d-none">
-                                <a class="nav-link active enlaceSN" style="width: 200px;" href="{{ route('general') }}">Mostrar datos de los Usuarios</a>
-                            </li>
-                            <li id="reportLink" class="nav-item d-none">
-                                <a class="nav-link active enlaceSN" style="width: 200px;" href="{{ route('perfil') }}">Mostrar Reporte</a>
-                            </li>
-                        </nav>
-                    <x-general-header />
-            <button id="toggle-dark-mode" class="btn btn-secondary printButtonClass" style="margin-left: 100px;"><i class="fa-solid fa-moon"></i>&nbspModo Obscuro</button>
+        <div class="container mt-4">
+            <!-- Selector para elegir el formulario -->
+            <label for="formSelect">Seleccionar Formulario:</label>
+            <select id="formSelect" class="form-select">
+                <option value=""></option>
+                <option value="form2">1. Permanencia en las actividades de la docencia</option>
+                <option value="form2_2">2. Dedicación en el desempeño docente</option>
+                <option value="form3_1">  3.1 Participación en actividades de diseño curricular</option>
+                <option value="form3_2">  3.2 Calidad del desempeño docente evaluada por el alumnado</option>
+                <option value="form3_3">  3.3 Publicaciones relacionadas con la docencia</option>
+                <option value="form3_4">  3.4 Distinciones académicas recibidas por el docente</option>
+                <option value="form3_5">  3.5 Asistencia, puntualidad y permanencia en el desempeño docente, evaluada por el JD y por CAAC</option>
+                <option value="form3_6">  3.6 Capacitación y actualización pedagógica recibida</option>
+                <option value="form3_7">  3.7 Cursos de actualización disciplinaria recibidos dentro de su área de conocimiento</option>
+                <option value="form3_8">  3.8 Impartición de cursos, diplomados, seminarios, talleres extracurriculares, de educación, continua o de formación y
+                capacitación docente</option>
+                <option value="form3_8_1"> 3.8.1 RSU</option>
+                <option value="form3_9">  3.9 Trabajos dirigidos para la titulación de estudiantes</option>
+                <option value="form3_10"> 3.10 Tutorías a estudiantes</option>
+                <option value="form3_11"> 3.11 Asesoría a estudiantes</option>
+                <option value="form3_12"> 3.12 Publicaciones de investigación relacionadas con el contenido de los PE que imparte el docente</option>
+                <option value="form3_13"> 3.13 Proyectos académicos de investigación</option>
+                <option value="form3_14"> 3.14 Participación como ponente en congresos o eventos académicos del Área de Conocimiento o afines del docente</option>
+                <option value="form3_15"> 3.15 Participación como ponente en congresos o eventos académicos del Área de Conocimiento o afines del docente</option>
+                <option value="form3_16"> 3.16 Registro de patentes y productos de investigación tecnológica y educativa</option>
+                <option value="form3_17"> 3.17 Actividades de arbitraje, revisión, correción y edición</option>
+                <option value="form3_18"> 3.18 Proyectos académicos de extensión y difusión</option>
+                <option value="form3_19"> 3.19 Organización de congresos o eventos institucionales del área de conocimiento de la o el Docente</option>
 
-                    <div class="container mt-4">
-                        <!-- Selector para elegir el formulario -->
-                        <label for="formSelect">Seleccionar Formulario:</label>
-                        <select id="formSelect" class="form-select">
-                            <option value=""></option>
-                            <option value="form2">1. Permanencia en las actividades de la docencia</option>
-                            <option value="form2_2">2. Dedicación en el desempeño docente</option>
-                            <option value="form3_1">  3.1 Participación en actividades de diseño curricular</option>
-                            <option value="form3_2">  3.2 Calidad del desempeño docente evaluada por el alumnado</option>
-                            <option value="form3_3">  3.3 Publicaciones relacionadas con la docencia</option>
-                            <option value="form3_4">  3.4 Distinciones académicas recibidas por el docente</option>
-                            <option value="form3_5">  3.5 Asistencia, puntualidad y permanencia en el desempeño docente, evaluada por el JD y por CAAC</option>
-                            <option value="form3_6">  3.6 Capacitación y actualización pedagógica recibida</option>
-                            <option value="form3_7">  3.7 Cursos de actualización disciplinaria recibidos dentro de su área de conocimiento</option>
-                            <option value="form3_8">  3.8 Impartición de cursos, diplomados, seminarios, talleres extracurriculares, de educación, continua o de formación y
-                            capacitación docente</option>
-                            <option value="form3_8_1"> 3.8.1 RSU</option>
-                            <option value="form3_9">  3.9 Trabajos dirigidos para la titulación de estudiantes</option>
-                            <option value="form3_10"> 3.10 Tutorías a estudiantes</option>
-                            <option value="form3_11"> 3.11 Asesoría a estudiantes</option>
-                            <option value="form3_12"> 3.12 Publicaciones de investigación relacionadas con el contenido de los PE que imparte el docente</option>
-                            <option value="form3_13"> 3.13 Proyectos académicos de investigación</option>
-                            <option value="form3_14"> 3.14 Participación como ponente en congresos o eventos académicos del Área de Conocimiento o afines del docente</option>
-                            <option value="form3_15"> 3.15 Participación como ponente en congresos o eventos académicos del Área de Conocimiento o afines del docente</option>
-                            <option value="form3_16"> 3.16 Registro de patentes y productos de investigación tecnológica y educativa</option>
-                            <option value="form3_17"> 3.17 Actividades de arbitraje, revisión, correción y edición</option>
-                            <option value="form3_18"> 3.18 Proyectos académicos de extensión y difusión</option>
-                            <option value="form3_19"> 3.19 Organización de congresos o eventos institucionales del área de conocimiento de la o el Docente</option>
+            </select>
+        </div>
 
-                        </select>
-                    </div>
-
-                    <div id="formContainer">
-                        <!-- Aquí se cargará el contenido del formulario seleccionado -->
-                        </div>
+        <div id="formContainer">
+            <!-- Aquí se cargará el contenido del formulario seleccionado -->
+            </div>
 
         @else
             <p>No tienes permisos para ver esta página.</p>
